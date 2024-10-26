1991cdd02d1ba2c81d2d6544068d84564c1f43d6
amanihushieh <amani.s.hosheh@gmail.com>
2024-10-26 15:55:32 +0300

    home page

--- category.js
+++ category.js
@@ -1,15 +1,9 @@
-window.addEventListener("load", function () {
-  setTimeout(function () {
-    document.getElementById("splash-screen").style.display = "none";
-    document.querySelector(".container").style.display = "block";
-  }, 3000);
+const startTest = document.getElementById("start-btn");
 
-  const startTest = document.getElementById("start-btn");
+startTest.addEventListener("click", function () {
+  const testCategory = document.getElementById("test-category").value;
 
-  startTest.addEventListener("click", function () {
-    const testCategory = document.getElementById("test-category").value;
-
-    localStorage.setItem("selectedCategory", testCategory);
-    window.location.href = "index.html";
-  });
+  localStorage.setItem("selectedCategory", testCategory);
+  window.location.href = "index.html";
 });
+
